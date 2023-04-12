15791f9f02d49950ace58a5ded9d037e3c9e80e8
Oliver Sharif <oliver@sharif-m.de>
2023-04-12 22:59:52 +0200

    :hammer: refactored workerpool

--- workerpool/workerpool.go
+++ workerpool/workerpool.go
@@ -6,16 +6,12 @@ import "sync"
 type Payload struct {
 	// The function to be executed
 	F func(...any) any
+	// The adress of the result
+	R any
 	// The Data to be sent to the functions
 	D []interface{}
 	//a waitgroup pointer to know when all the work is done
 	Wg *sync.WaitGroup
-}
-
-// The ResultSet is the result of the worker threads
-type ResultSet struct {
-	// The results of the worker threads
-	Results interface{}
 }
 
 // The WorkerPool is the pool with goroutines
@@ -25,8 +21,6 @@ type WorkerPool struct {
 	NumWorkers int
 	// Channel for the worker threads
 	In chan Payload
-	// Channel for the worker threads
-	Out chan ResultSet
 }
 
 // NewWorkerPool creates a new worker pool
@@ -35,7 +29,6 @@ func NewWorkerPool(numWorkers int) *WorkerPool {
 	wp := &WorkerPool{
 		NumWorkers: numWorkers,
 		In:         make(chan Payload),
-		Out:        make(chan ResultSet),
 	}
 
 	// Start the worker threads
@@ -45,9 +38,7 @@ func NewWorkerPool(numWorkers int) *WorkerPool {
 				// Get the payload
 				payload := <-wp.In
 				// Execute the functions on the data
-				result := payload.F(payload.D...)
-				// Send the result back
-				wp.Out <- ResultSet{Results: result}
+				payload.R = payload.F(payload.D...)
 				// Signal that the work is done
 				payload.Wg.Done()
 			}
